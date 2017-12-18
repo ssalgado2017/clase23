50abb4bd81d90f958990cdddca403de2ab4d925d
Sandra <sandra.salgado@bci.cl>
2017-12-18 19:00:20 -0300

    Agrega assets al asset path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,12 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( *.woff *.eot *.svg *.ttf)
